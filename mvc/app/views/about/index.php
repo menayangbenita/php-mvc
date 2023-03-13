bf1cdf6b0699aa9aa998e518d84ca2007b38d71a
benita alexandra <124318206+menayangbenita@users.noreply.github.com>
2023-03-13 21:17:26 +0700

    Add files via upload

---
+++ mvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="mt-5">
+        <h1> About Me </h1>
+        <img src="<?=BASEURL;?>/img/olivia.jpg" alt="Profile" width="200" class="rounded-circle shadow mt-4" > </br>
+        <p class="mt-4"> Halo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?> tahun, saya adalah seorang <?= $data['pekerjaan'];?></p>
+    </div>
+</div>
